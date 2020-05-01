5bc842a18e5d22cb2b6fb6a786cc3e877d60bd31
1bastien1 <macgrey0000@gmail.com>
2020-05-01 20:58:14 +0200

    refacto home component

--- components/Home.js
+++ components/Home.js
@@ -18,6 +18,7 @@ export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
+        
         <ImageBackground
           source={require('../assets/seedTest.jpg')}
           style={styles.backgroundImage}>
@@ -49,96 +50,6 @@ export default class Home extends Component {
     //removeItemApp();
     saveNewCalendar();
     getAutorisations();
-    this.getLocalisationAndWeather();
-  }
-
-  /* Example location returned
-    {
-      speed: -1,
-      longitude: -0.1337,
-      latitude: 51.50998,
-      accuracy: 5,
-      heading: -1,
-      altitude: 0,
-      altitudeAccuracy: -1
-      floor: 0
-      timestamp: 1446007304457.029,
-      fromMockProvider: false
-    }
-  */
-  /**
-   * setState the localisation {lat, long} and save it in asyncStorage (key: localisation) only if not already defined in component state
-   */
-  async getLocalisationAndWeather() {
-    if (this.state.localisation != undefined) {
-      await RNLocation.requestPermission({
-        ios: 'whenInUse',
-        android: {
-          detail: 'coarse',
-        },
-      })
-        .then((response) => {
-          return JSON.parse(response);
-        })
-        .then((granted) => {
-          if (granted) {
-            this.locationSubscription = RNLocation.subscribeToLocationUpdates(
-              (locations) => {
-                console.log('location : ', locations[0].longitude);
-                _storeData(
-                  'localisation',
-                  JSON.stringify({
-                    longitude: locations[0].longitude,
-                    latitude: locations[0].latitude,
-                  }),
-                );
-                this.setState({
-                  localisation: {
-                    longitude: locations[0].longitude,
-                    latitude: locations[0].latitude,
-                  },
-                });
-                this.getWeather(locations[0].latitude, locations[0].longitude);
-              },
-            );
-          }
-        });
-    }
-  }
-
-  /**
-   * setState the weather
-   */
-  getWeather(lat, long) {
-    let tryUrl =
-      'http://api.openweathermap.org/data/2.5/weather?lat=' +
-      lat +
-      '&lon=' +
-      long +
-      '&appid=8bfd18a82183435001ca3f38713a00a5';
-    fetch(tryUrl)
-      .then((json) => json.json())
-      .then(
-        (weather) => {
-          console.log('weather : ', weather);
-          this.setState({weather: weather});
-          _storeData('weather', JSON.stringify(weather));
-        },
-        (err) => {
-          console.log(
-            'error fetch weather : ',
-            err,
-            '//',
-            'api.openweathermap.org/data/2.5/weather?lat=' +
-              lat +
-              '&lon=' +
-              long +
-              '&appid=8bfd18a82183435001ca3f38713a00a5&lang=fr',
-          );
-        },
-      );
-    //api key 8bfd18a82183435001ca3f38713a00a5
-    //api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8bfd18a82183435001ca3f38713a00a5
   }
 }
 
